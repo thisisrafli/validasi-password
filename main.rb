0e031fe6952a63da870ddcde1a9e0cc5e7a48d52
Rafli <adebae434@gmail.com>
2026-01-14 17:58:00 +0700

    update main.rb

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@
 
 def validate_password(password)
    
-   if password.length > 8 and password.match?(/[A-Z]/) and password.match?(/[a-z]/) and password.match?(/[0-9]/) and password.match?(/[!@#$%^&*()_=+\-\[\]{};:'",.<>\/?\\|]/)
+   if password.length > 10 and password.match?(/[A-Z]/) and password.match?(/[a-z]/) and password.match?(/[0-9]/) and password.match?(/[!@#$%^&*()_=+\-\[\]{};:'",.<>\/?\\|]/)
       return true
    end
    
